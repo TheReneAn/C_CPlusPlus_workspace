9e14ec0e1d93653ef246a73e4090604c027cb410
YoujinAn <auj0827@gmail.com>
2020-12-16 20:40:54 -0500

    Constructor And Destrucor

---
+++ ConstructorAndDestrucor/ObjectPrinterArray/ObjPrtArr.cpp
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Person {
+private:
+	char* name;
+	int age;
+public:
+	Person(const char* myname, int myage) {
+		// Name
+		int len = strlen(myname) + 1;
+		name = new char[len];
+		strcpy(name, myname);
+
+		// Age
+		age = myage;
+	}
+
+	Person()
+	{
+		name = NULL;
+		age = 0;
+		cout << "Called Person()" << endl;
+	}
+
+	void SetPersonInfo(char* myname, int myage) {
+		name = myname;
+		age = myage;
+	}
+
+	void ShowPersonInfo() const
+	{
+		cout << "Name: " << name << ", ";
+		cout << "Age: " << age << endl;
+	}
+
+	~Person()
+	{
+		delete[]name;
+		cout << "Called destructor!" << endl;
+	}
+};
+
+int main(void)
+{
+	Person* parr[3];
+	char namestr[100];
+	int age;
+
+	for (int i = 0; i < 3; i++) {
+		cout << "Name: ";
+		cin >> namestr;
+		cout << "Age: ";
+		cin >> age;
+
+		parr[i] = new Person(namestr, age);
+	}
+
+	parr[0]->ShowPersonInfo();
+	parr[1]->ShowPersonInfo();
+	parr[2]->ShowPersonInfo();
+
+	delete parr[0];
+	delete parr[1];
+	delete parr[2];
+
+	return 0;
+}
